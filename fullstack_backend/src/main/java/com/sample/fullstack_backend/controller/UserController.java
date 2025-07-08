906cbeecaed5e80bf0ada9cc2a56632967a1ff33
Hridaya Patil <hridaya.sudhirpatil@gmail.com>
2025-07-08 17:24:26 +0530

    changes

--- fullstack_backend/src/main/java/com/sample/fullstack_backend/controller/UserController.java
+++ fullstack_backend/src/main/java/com/sample/fullstack_backend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.sample.fullstack_backend.controller;
 import com.sample.fullstack_backend.model.User;
 import com.sample.fullstack_backend.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepo UserRepo;
